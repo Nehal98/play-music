3195ff0f74c3ded94b2c472eca6994f112c442a5
nehal <nehalahmad2011@gmail.com>
2021-09-17 14:54:18 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Heading from './components/header/heading';
+import Roomcards from './components/roomcards/roomcards';
+
+function App() {
+  return (
+    
+     <><Heading />
+     <Roomcards />
+     </>
+    )
+    
+  
+}
+
+export default App
